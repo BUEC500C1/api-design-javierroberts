5007e3223d3edfc6dc65f65073582cee881e512c
javierroberts <javier.roberts@hotmail.com>
2020-02-05 11:25:33 -0500

    Added code for annotating images using google vision

--- feedTools.py
+++ feedTools.py
@@ -1,5 +1,7 @@
 import tweepy
 import keys
+from google.cloud import vision
+import io
 
 
 def getFeed():
@@ -13,5 +15,27 @@ def getFeed():
         print(tweet.text)
 
 
+# def annotateImage():
+#     client = vision.ImageAnnotatorClient()
+#     path = "test_image.png"
+
+#     with io.open(path, 'rb') as image_file:
+#         content = image_file.read()
+
+#     image = vision.types.Image(content=content)
+
+#     response = client.label_detection(image=image)
+#     labels = response.label_annotations
+#     print('Labels:')
+
+#     for label in labels:
+#         print(label.description)
+
+#     if response.error.message:
+#         raise Exception(
+#             '{}\nFor more info on error messages, check: '
+#             'https://cloud.google.com/apis/design/errors'.format(
+#                 response.error.message))
+
+
 if __name__ == "__main__":
-    getFeed()
