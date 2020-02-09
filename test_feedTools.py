5f5307371bf52c37e610898ca67db9d98b7d28d3
javierroberts <javier.roberts@hotmail.com>
2020-02-08 23:51:23 -0500

    Removed testing functions because they all require keys, which cannot be uploaded to GitHub

--- test_feedTools.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import feedTools
-
-
-# Testing that the annotateImage() function is correctly annotating images with Googles Vision API.
-def test_imageVision():
-    assert feedTools.annotateImage(
-        "https://pbs.twimg.com/media/EQTfQDoUwAIMsRY?format=jpg&name=4096x4096") == "Basketball player, Basketball, Basketball moves, Player, Sports, Team sport, Basketball court, Ball game, Tournament, Muscle"
